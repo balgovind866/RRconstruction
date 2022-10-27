23f737769969ec011ceddf0655d895440966f2f7
bal govind maurya <95010598+balgovind866@users.noreply.github.com>
2022-10-27 17:54:33 +0530

    third commit when app image picker

--- lib/pagelogin/add_project.dart
+++ lib/pagelogin/add_project.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProjectAdd extends StatefulWidget {
   const ProjectAdd({Key? key}) : super(key: key);
@@ -11,147 +14,270 @@ class ProjectAdd extends StatefulWidget {
 }
 
 class _ProjectAddState extends State<ProjectAdd> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldMessengerState> _scafoldkey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  double? price = 0.0;
+  int? Quantity = 0;
+  String? product_name = '';
+  String? description = '';
+
+  void uploadproduct() {
+    if (_formkey.currentState!.validate()) {
+      _formkey.currentState?.save();
+      print('valid all requerment');
+      print(price);
+      print(Quantity);
+      print(product_name);
+      print(description);
+    } else {
+      _scafoldkey.currentState?.showSnackBar(SnackBar(
+        content: Text(
+          'fill all the requarment ',
+          style: TextStyle(color: Colors.black),
+        ),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.yellow,
+      ));
+    }
+  }
+
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? imagesFileList = [];
+  dynamic _pickedImageError;
+
+  void pickProductImages() async {
+    try {
+      final pickedImages = await _picker.pickMultiImage(
+          maxHeight: 400, maxWidth: 400, imageQuality: 95);
+      setState(() {
+        imagesFileList = pickedImages;
+      });
+    } catch (e) {
+      setState(() {
+        _pickedImageError = e;
+      });
+      print(_pickedImageError);
+    }
+  }
+
+  Widget previewImages() {
+    if (imagesFileList!.isNotEmpty) {
+      return ListView.builder(
+          itemCount: imagesFileList!.length,
+          itemBuilder: (context, index) {
+            return Image.file(File(imagesFileList![index].path));
+          });
+    } else {
+      return const Center(
+          child: const Center(
+        child: Text(
+          'you have not \n\n picked image yet ! ',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16),
+        ),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          reverse: true,
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+    return ScaffoldMessenger(
+      key: _scafoldkey,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            reverse: true,
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Form(
+              autovalidateMode: AutovalidateMode.always,
+              key: _formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Stack(children: [
+                        Container(
+                          color: Colors.blueGrey.shade100,
+                          height: MediaQuery.of(context).size.width * 0.5,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: imagesFileList != null
+                              ? previewImages()
+                              : const Center(
+                                  child: Text(
+                                    'you have not \n\n picked image yet ! ',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                        ),
 
-            Row(children: [
-              Container(
-                color: Colors.blueGrey.shade100,
-                height: MediaQuery.of(context).size.width *0.5,
-                width: MediaQuery.of(context).size.width *0.5,
-                child: const Center(child: Text('you have not \n\n picked image yet ! ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                ),),
-
-              )
-            ],),
-               Padding(
-                 padding:EdgeInsets.all(8),
-                 child: SizedBox(
-                   height: 30.h,
-                   child: Divider(
-                      color: Colors.yellow,
-                     thickness: 1.5,
+                      ]),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SizedBox(
+                      height: 30.h,
+                      child: Divider(
+                        color: Colors.yellow,
+                        thickness: 1.5,
+                      ),
                     ),
-                 ),
-               ),
-            Padding(
-              padding:EdgeInsets.all(8),
-
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width*0.4,
-                child: TextFormField(
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-                  decoration: textFormDecoration.copyWith(
-                  labelText: 'price',
-                  hintText: 'price...\$',
-                ),
-                ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "pleas enter price";
+                          } else if (value.isValidQuantity() != true) {
+                            return 'not valid quantity';
+                          } else {
+                            return null;
+                          }
+                        },
+                        onSaved: (value) {
+                          price = double.parse(value!);
+                        },
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'price ',
+                          hintText: 'price...\$',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "pleas enter quantity";
+                          } else if (value.isValidQuantity() != true) {
+                            return 'not valid quantity';
+                          } else {
+                            return null;
+                          }
+                        },
+                        onSaved: (val) {
+                          Quantity = int.parse(val!);
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'Quantity',
+                          hintText: 'Aad quantity',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        onSaved: (value) {
+                          product_name = value;
+                        },
+                        maxLength: 100,
+                        maxLines: 3,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'product name',
+                          hintText: 'Enter product name',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        onSaved: (value) {
+                          description = value;
+                        },
+                        maxLength: 800,
+                        maxLines: 6,
+                        decoration: textFormDecoration.copyWith(
+                          labelText: 'product description',
+                          hintText: 'Enter product description',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-
-              Padding(
-                padding:EdgeInsets.all(8),
-
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width*0.4,
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: textFormDecoration.copyWith(
-                    labelText: 'Quantity',
-                    hintText: 'Aad quantity',
-                  ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding:EdgeInsets.all(8),
-
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                    maxLength: 100,
-                    maxLines: 3,
-                    decoration: textFormDecoration.copyWith(
-                    labelText: 'product name',
-                    hintText: 'Enter product name',
-                  ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding:EdgeInsets.all(8),
-
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                    maxLength: 800,
-                    maxLines: 6,
-                    decoration: textFormDecoration.copyWith(
-                    labelText: 'product description',
-                    hintText: 'Enter product description',
-                  ),
-                  ),
-                ),
-              ),
-            ],
           ),
         ),
-      ),
-      floatingActionButton:Row (
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: FloatingActionButton(onPressed:(){},
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: FloatingActionButton(
+                onPressed: imagesFileList!.isEmpty
+                    ? () {
+                        pickProductImages();
+                      }
+                    : () {
+                        setState(() {
+                          imagesFileList = [];
+                        });
+                      },
+                backgroundColor: Colors.yellow,
+                child: imagesFileList!.isEmpty
+                    ? Icon(
+                        Icons.photo_library,
+                        color: Colors.black,
+                      )
+                    : Icon(
+                        Icons.delete_forever,
+                        color: Colors.black,
+                      ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: uploadproduct,
               backgroundColor: Colors.yellow,
               child: Icon(
-                Icons.photo_library,
+                Icons.upload,
                 color: Colors.black,
               ),
-
             ),
-          ),
-          FloatingActionButton(onPressed:(){},
-            backgroundColor: Colors.yellow,
-            child: Icon(
-               Icons.upload,
-              color: Colors.black,
-            ),
-
-          ),
-        ],
-
+          ],
+        ),
       ),
-
-
     );
   }
 }
-var textFormDecoration=InputDecoration(
-  labelText: 'price',hintText: 'price...\$',
-  labelStyle: TextStyle(color: Colors.blue),
 
+var textFormDecoration = InputDecoration(
+  labelText: 'price',
+  hintText: 'price...\$',
+  labelStyle: TextStyle(color: Colors.blue),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
   ),
   enabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.yellow,width: 1),
+      borderSide: BorderSide(color: Colors.yellow, width: 1),
       borderRadius: BorderRadius.circular(10)),
   focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.indigoAccent,width: 2),
-      borderRadius: BorderRadius.circular(10),
-
+    borderSide: BorderSide(color: Colors.indigoAccent, width: 2),
+    borderRadius: BorderRadius.circular(10),
   ),
 );
 
-
+extension QuantityValidator on String {
+  bool isValidQuantity() {
+    return RegExp(r'^((([1-9][0-9]*[\.]*)||([0][\.]*))([0-9]{1,2}))$')
+        .hasMatch(this);
+  }
+}
